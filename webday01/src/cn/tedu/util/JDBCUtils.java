78dcc730359a5a9ce334b10337730aa07034114c
蚕尹 <1543839509@qq.com>
2018-08-01 17:48:09 +0800

    webday01
    在第二天的时候，对一部分文件进行了优化

--- webday01/src/cn/tedu/util/JDBCUtils.java
+++ webday01/src/cn/tedu/util/JDBCUtils.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ResourceBundle;
 
 /**
  * 这个类用来完成jdbc工具类的开发
@@ -19,12 +20,16 @@ public class JDBCUtils {
 	//6.2.2提供静态方法getConnection。用来对外提供数据连接对象
 	public static Connection getConnection(){
 		try {
+			
+			//读取属性文件
+			ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+			
 			//1.注册驱动
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(rb.getString("driverClass"));
 			//2.提供数据库连接
-			String url = "jdbc:mysql:///jt_bd";//设置数据库
-			String user = "root";//设置数据库的用户名
-			String password = "root";//设置用户名的密码
+			String url = rb.getString("jdbcUrl");//设置数据库
+			String user = rb.getString("user");//设置数据库的用户名
+			String password = rb.getString("password");//设置用户名的密码
 			Connection conn = DriverManager.getConnection(url, user, password);
 			
 			return conn;
